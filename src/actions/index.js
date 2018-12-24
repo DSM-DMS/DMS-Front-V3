10129e6d9f16ac7b33f93014b473e1057c072bb3
gtobio11 <gtobio11@gmail.com>
2018-12-24 18:29:45 +0900

    init

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,17 @@
 // 아래는 예제
 export const INCREMENT = 'INCREMENT'; // 이와같이 액션 명 정의
+export const SET_DIFF = 'SET_DIFF';
 
-export function increment() {
+export function increment(value) {
     return {
-        type: INCREMENT
+        type: INCREMENT,
+        index: value
     };
 } // 22
+
+export function setDiff(value) {
+    return {
+        type: SET_DIFF,
+        diff: value
+    }
+} // dispatch 할때 인자를 호출하면
