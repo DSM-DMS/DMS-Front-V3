d05dd199cce94d820723c1c9d4d6af739119978e
khg0712 <k7120792@gmail.com>
2019-01-04 23:57:09 +0900

    [ADD] apply detail main container content switching

--- src/student/container/Apply/ApplyMainContainer.js
+++ src/student/container/Apply/ApplyMainContainer.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 
 import MenuContainer from './Menu/MenuContainer';
+import ExtensionApplyContainer from './Content/ExtensionApplyContainer'
+import GoingoutApplyContainer from './Content/GoingoutApplyContainer'
+import MusicApplyContainer from './Content/MusicApplyContainer'
+import StayApplyContainer from './Content/StayApplyContainer'
 
 class ApplyMainContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            path: this.props.location.pathname
+        }
+    }
+
     render() {
+        const {path} = this.state;
+        const type = path.substring(7, path.length);
+        const containerType = {
+            extension: <ExtensionApplyContainer/>,
+            goingout: <GoingoutApplyContainer/>,
+            music: <MusicApplyContainer/>,
+            stay: <StayApplyContainer/>
+        }
+        const content = containerType[type];
+
         return (
             <div style={{ width: window.screen.width}} className="apply--main--wrapper">
-                <MenuContainer />
+                <MenuContainer/>
+                {content}
             </div>
         )
     }
