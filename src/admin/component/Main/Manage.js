32c7a041f2ab8704b111f63107c2721d09e07475
unknown <leaf_cis@naver.com>
2019-01-09 15:47:08 +0900

    [FIX] ERROR

--- src/admin/component/Main/Manage.js
+++ src/admin/component/Main/Manage.js
@@ -2,21 +2,13 @@ import React from 'react';
 
 import './Manage.scss'
 
-<<<<<<< HEAD
-import gears from '../../../assets/icon/ic_gears.png'
 
-=======
->>>>>>> a78b4390810b356f1fca223edd605b069b6888e9
 const Manage = ({cardList}) => {
     return (
         <div className = "manage--wrapper">
             <div className = "manage--inner--wrapper">
                 <div className = "manage--logo--wrapper">
-<<<<<<< HEAD
-                    <img className = "manage--logo--icon" src = {gears} alt = "gears"/>
-=======
                     <img className = "manage--logo--icon"/>
->>>>>>> a78b4390810b356f1fca223edd605b069b6888e9
                     <div className = "manage--logo--text">
                         Management
                     </div>
