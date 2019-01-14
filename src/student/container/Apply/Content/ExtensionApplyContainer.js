13b3fce48fea3773a58e45a048323ec9fdea0f6b
khg0712 <k7120792@gmail.com>
2019-01-14 16:14:24 +0900

    [ADD][MODIFY] 신청 content 파트추가, menu css 수정

--- src/student/container/Apply/Content/ExtensionApplyContainer.js
+++ src/student/container/Apply/Content/ExtensionApplyContainer.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import './ExtensionApplyContainer.scss'
 
-class ExtensionApplyContainer extends Component {
+import ApplyExtensionBtn from '../../../component/Apply/content/ApplyExtensionBtn'
+import ApplyExtensionMap from '../../../component/Apply/content/ApplyExtensionMap'
+import ApplyAcceptBtn from '../../../component/Apply/content/ApplyAcceptBtn';
+
+export default class ExtensionApplyContainer extends Component {
     render() {
-        return (
-            <div>
+        const btns = ['가온실', '나온실', '다온실', '라온실', '2층 여자 독서실', '3층 계단측 독서실', '3층 학교측 독서실', '4층 계단측 독서실', '4층 학교측 독서실', '5층 열린 교실'];
 
+        const btnList = btns.map((content) => (<ApplyExtensionBtn content={content}/>));
+
+        return (
+            <div className = 'apply--extension--wrapper'>
+                <p className = 'apply--extension--title'>연장학습 신청</p>
+                <div className = 'apply--extension--btnlist'>
+                    {btnList}
+                </div>
+                <ApplyExtensionMap/>
+                <div className = 'apply--extension--accept--btnlist'>
+                    <ApplyAcceptBtn type = 'apply--extension--cancle--btn' title = '취소'/>
+                    <ApplyAcceptBtn type = 'apply--extension--accept--btn' title = '신청'/>
+                </div>
             </div>
         )
     }
 }
-
-export default ExtensionApplyContainer;
