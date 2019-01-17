1ab156c95c1b72cf077dbf1ad8914b841bf4643a
khg0712 <k7120792@gmail.com>
2019-01-17 09:54:49 +0900

    [ADD] 외출 신청 카드 뷰 추가

---
+++ src/student/component/Apply/content/goingout/GoingoutCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './GoingoutCard.scss'
+
+const GoingoutCard = ({gooutDate, returnDate, reason}) => {
+    const gooutTime = gooutDate.substr(10, 6);
+    const returnTime = returnDate.substr(10, 6);
+    const date = `${gooutDate[5]}${gooutDate[6]}월 ${gooutDate[8]}${gooutDate[9]}일`;
+    
+    return (
+        <div className = 'apply--goingout--card'>
+            <p className = 'apply--goingout--card--title'>외출 신청서</p>
+            <p className = 'apply--goingout--card--time'>{`${date} ${gooutTime} ~ ${returnTime}`}</p>
+        </div>
+    )
+}
+
+export default GoingoutCard;
