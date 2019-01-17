e200370273628e9b72160a3d39857d6269129a86
khg0712 <k7120792@gmail.com>
2019-01-17 16:43:19 +0900

    [ADD] 기상음악 신청 카드뷰 추가

---
+++ src/student/component/Apply/content/music/AppliedMusicCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './AppliedMusicCard.scss'
+
+const AppliedMusicCard = ({title, singer, author}) => {
+    return (
+        <div class = 'apply--music--card'>
+            <p className = 'apply--music--card--title'>{title}</p>
+            <p className = 'apply--music--card--singer'>{singer}</p>
+            <p className = 'apply--music--card--author'>{author}</p>
+        </div>
+    );
+}
+
+export default AppliedMusicCard;
