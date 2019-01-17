ee9ebbd9a53ea552420e06fa899cd1cb7eedd7ef
khg0712 <k7120792@gmail.com>
2019-01-17 09:53:36 +0900

    [ADD] 연장학습 신청 좌석 표 추가(demo)

---
+++ src/student/component/Apply/content/ApplyExtensionMap.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ApplyExtensionMap.scss'
+
+export default class ApplyExtensionMap extends Component {
+    render() {
+        return (
+            <div className = 'apply--extension--map'>
+
+            </div>
+        )
+    }
+}
