d6a5462b66cdb40026b4f1c97c3e8d1f4c0092f8
khg0712 <k7120792@gmail.com>
2019-01-18 15:20:08 +0900

    [DELETE] 리덕스 예제 코드 삭제

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,22 +7,3 @@ export function setSection(value) {
         currentSection: value
     }
 }
-
-
-// 아래는 예제
-export const INCREMENT = 'INCREMENT'; // 이와같이 액션 명 정의
-export const SET_DIFF = 'SET_DIFF';
-
-export function increment(value) {
-    return {
-        type: INCREMENT,
-        index: value
-    };
-} // 22
-
-export function setDiff(value) {
-    return {
-        type: SET_DIFF,
-        diff: value
-    }
-} // dispatch 할때 인자를 호출하면
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router'
-import counter from './counter';
+
 import section from './section';
 
 
 const dmsApp = (history) => combineReducers({
-    counter,
     // 분할한 리듀서 사용 
     // import를 통해서 불러오며 직관적인 데이터 관계가 없을 시 새로운 파일로 작성
     section,
