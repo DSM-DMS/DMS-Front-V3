ffc73255f52af8b368641b1587c63bfd81805d4d
khg0712 <k7120792@gmail.com>
2019-01-18 11:43:33 +0900

    [ADD] 기상음악신청카드 class => className 수정

--- src/student/component/Apply/content/music/AppliedMusicCard.js
+++ src/student/component/Apply/content/music/AppliedMusicCard.js
@@ -4,7 +4,7 @@ import './AppliedMusicCard.scss'
 
 const AppliedMusicCard = ({title, singer, author}) => {
     return (
-        <div class = 'apply--music--card'>
+        <div className = 'apply--music--card'>
             <p className = 'apply--music--card--title'>{title}</p>
             <p className = 'apply--music--card--singer'>{singer}</p>
             <p className = 'apply--music--card--author'>{author}</p>
