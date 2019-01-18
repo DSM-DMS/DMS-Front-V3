f9b1dcd2efb128262a31cacb3db793b44b81e407
khg0712 <k7120792@gmail.com>
2019-01-18 11:40:49 +0900

    [ADD] 신청 추가 카드 추가(Demo)

---
+++ src/student/component/Apply/content/utils/ApplyAddCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ApplyAddCard.scss'
+
+const ApplyAddCard = () => {
+    return (
+        <div className = 'apply--add--card'>
+            <p className = 'apply--add--card--text'>눌러서 신청</p>
+        </div>
+    )
+}
+
+export default ApplyAddCard;
