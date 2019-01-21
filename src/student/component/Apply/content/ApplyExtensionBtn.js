31409614b3372f7ab7ca8f4c22177384728d83d7
khg0712 <k7120792@gmail.com>
2019-01-21 09:30:18 +0900

    [MODIFY] 신청 페이지 타입 버튼 class 컴포넌트 => functional 컴포넌트 전환

--- src/student/component/Apply/content/ApplyExtensionBtn.js
+++ src/student/component/Apply/content/ApplyExtensionBtn.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ApplyExtensionBtn.scss'
 
-export default class ApplyExtensionBtn extends Component {
-    render() {
-        const {content, selected, onChangeType} = this.props;
-        return (
-            <div className = {`apply--extens--btn ${selected}`} onClick = {onChangeType.bind(null, content)}>{content}</div>
-        )
-    }
+const ApplyExtensionBtn = ({content, selected, onChangeType}) => {
+    return (
+        <div className = {`apply--extens--btn ${selected}`} onClick = {onChangeType.bind(null, content)}>{content}</div>
+    )
 }
+
+export default ApplyExtensionBtn;
