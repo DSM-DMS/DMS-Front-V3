cfc6b6a15d9442ea163a74edaa8e1d3887b86eb2
khg0712 <k7120792@gmail.com>
2019-01-21 11:01:36 +0900

    [DELETE] 불필요한 withRouter 삭제

--- src/student/container/Apply/ApplyMainContainer.js
+++ src/student/container/Apply/ApplyMainContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom';
 
 import ApplyMenuContainer from './Menu/ApplyMenuContainer';
 import ExtensionApplyContainer from './Content/ExtensionApplyContainer'
