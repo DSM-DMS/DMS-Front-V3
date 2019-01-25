666b5802bd09deaf94539c4464e487c9109ba21d
khg0712 <k7120792@gmail.com>
2019-01-25 15:54:15 +0900

    [MODIFY] apply music, goout code prettier, modify layout of music

--- src/student/component/Apply/content/utils/ApplyAddCard.js
+++ src/student/component/Apply/content/utils/ApplyAddCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import './ApplyAddCard.scss';
 import applyAddImg from '../../../../../assets/icon/ic_apply_add.png';
@@ -6,7 +6,11 @@ import applyAddImg from '../../../../../assets/icon/ic_apply_add.png';
 const ApplyAddCard = () => {
   return (
     <div className='apply--add--card'>
-      <img className = 'apply--add--card--img' src = {applyAddImg} alt = '추가 아이콘'/>
+      <img
+        className='apply--add--card--img'
+        src={applyAddImg}
+        alt='추가 아이콘'
+      />
       <p className='apply--add--card--text'>눌러서 신청</p>
     </div>
   );
--- src/student/container/Apply/Content/MusicCardContainer.js
+++ src/student/container/Apply/Content/MusicCardContainer.js
@@ -1,39 +1,49 @@
-import React from 'react'
-import { connect } from "react-redux";
-import { selectMusicCard } from '../../../../actions/ApplyActions'
+import React from 'react';
+import { connect } from 'react-redux';
+import { selectMusicCard } from '../../../../actions/ApplyActions';
 
 import AppliedMusicCard from '../../../component/Apply/content/music/AppliedMusicCard';
-import ApplyAddCard from '../../../component/Apply/content/utils/ApplyAddCard'
+import ApplyAddCard from '../../../component/Apply/content/utils/ApplyAddCard';
 
 import './MusicCardContainer.scss';
 
-const MusicCardContainer = (props) => {
-    const { cardsInfo, musicCard, onChangeCard } = props;
-    const musicCards = cardsInfo.map((info, i) => {
-        let selectedClass;
-        if(info.id === musicCard)
-            selectedClass = 'apply--music--card--selected';
-        return (
-            <AppliedMusicCard title = {info.musicName} singer = {info.singer} author = {info.studentName} key = {info.id} id = {info.id} selectedClass = {selectedClass} onChangeCard = {onChangeCard}/>
-        )
-    })
-
-    const isThereAddCard = musicCards.length <= 4;
-
+const MusicCardContainer = props => {
+  const { cardsInfo, musicCard, onChangeCard } = props;
+  const musicCards = cardsInfo.map(info => {
+    let selectedClass;
+    if (info.id === musicCard) selectedClass = 'apply--music--card--selected';
     return (
-        <div className = 'apply--music--card--wrapper'>
-            {musicCards}
-            {isThereAddCard && <ApplyAddCard />}
-        </div>
-    )
-}
+      <AppliedMusicCard
+        title={info.musicName}
+        singer={info.singer}
+        author={info.studentName}
+        key={info.id}
+        id={info.id}
+        selectedClass={selectedClass}
+        onChangeCard={onChangeCard}
+      />
+    );
+  });
 
-const mapStateToProps = (state) => ({
-    musicCard: state.ApplyCardSwitch.musicCard
+  const isThereAddCard = musicCards.length <= 4;
+
+  return (
+    <div className='apply--music--card--wrapper'>
+      {musicCards}
+      {isThereAddCard && <ApplyAddCard />}
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  musicCard: state.ApplyCardSwitch.musicCard
 });
 
-const mapDispatchToProps = (dispatch) => ({
-    onChangeCard: (musicCard) => dispatch(selectMusicCard(musicCard))
+const mapDispatchToProps = dispatch => ({
+  onChangeCard: musicCard => dispatch(selectMusicCard(musicCard))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(MusicCardContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MusicCardContainer);
