398e9b69b53c34c1c09a464a1ee9d25df1f47fb1
khg0712 <k7120792@gmail.com>
2019-01-25 11:42:53 +0900

    [ADD & MODIFY] 외출 신청 아이콘, 배경 추가 및 카드 리스트 CSS flex => grid 레이아웃 변경

--- src/student/component/Apply/content/utils/ApplyAddCard.js
+++ src/student/component/Apply/content/utils/ApplyAddCard.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './ApplyAddCard.scss'
+import './ApplyAddCard.scss';
+import applyAddImg from '../../../../../assets/icon/ic_apply_add.png';
 
 const ApplyAddCard = () => {
-    return (
-        <div className = 'apply--add--card'>
-            <p className = 'apply--add--card--text'>눌러서 신청</p>
-        </div>
-    )
-}
+  return (
+    <div className='apply--add--card'>
+      <img className = 'apply--add--card--img' src = {applyAddImg} alt = '추가 아이콘'/>
+      <p className='apply--add--card--text'>눌러서 신청</p>
+    </div>
+  );
+};
 
 export default ApplyAddCard;
