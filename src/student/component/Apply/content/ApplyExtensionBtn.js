21c9b68e8a36609335f23c5bc3a45ad2cfd984cd
khg0712 <k7120792@gmail.com>
2019-02-07 21:42:11 +0900

    [MODIFY] extension btn onclick 메서드 추가, stay container  버튼 클릭구현

--- src/student/component/Apply/content/ApplyExtensionBtn.js
+++ src/student/component/Apply/content/ApplyExtensionBtn.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './ApplyExtensionBtn.scss'
 
-const ApplyExtensionBtn = ({content, selected, onChangeType}) => {
+const ApplyExtensionBtn = ({content, selected, onChangeType, val}) => {
     return (
-        <div className = {`unselectable apply--extens--btn ${selected}`} onClick = {onChangeType.bind(null, content)}>{content}</div>
+        <div className = {`unselectable apply--extens--btn ${selected}`} onClick = {onChangeType.bind(null, val)}>{content}</div>
     )
 }
 
