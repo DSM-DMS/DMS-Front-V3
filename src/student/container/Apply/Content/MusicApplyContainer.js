2cb97bf1530a8b0a1133f5fc9f11160daef15fb2
khg0712 <k7120792@gmail.com>
2019-02-07 21:43:53 +0900

    [MODIFY] music 통신 로직 구현 완료

--- src/student/container/Apply/Content/MusicApplyContainer.js
+++ src/student/container/Apply/Content/MusicApplyContainer.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setMusicDate } from '../../../../actions/ApplyActions';
 import { getMusicList, submitMusic } from '../../../../lib/applyAPI';
+import { getCookie } from '../../../../lib/cookie';
 
 import './MusicApplyContainer.scss';
 
@@ -10,120 +11,49 @@ import MusicCardContainer from './MusicCardContainer';
 import MusicSubmitCard from '../../../component/Apply/content/music/MusicSubmitCard';
 
 class MusicApplyContainer extends Component {
-  state = {
-    dayType: ['월요일', '화요일', '수요일', '목요일', '금요일'],
-    cardsInfo: {
-      fri: [
-        {
-          applyDate: '2019-01-12 11:27:25.689022',
-          id: 8,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '금요일의 누군가'
-        },
-        {
-          applyDate: '2019-01-23 11:27:25.689022',
-          id: 9,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '그묭일'
-        }
+  constructor(props) {
+    super(props);
+    console.log(getMusicList(getCookie('JWT')));
+    this.state = {
+      dayType: [
+        { date: '월요일', val: 'mon' },
+        { date: '화요일', val: 'tue' },
+        { date: '수요일', val: 'wed' },
+        { date: '목요일', val: 'thu' },
+        { date: '금요일', val: 'fri' }
       ],
-      mon: [
-        {
-          applyDate: '2019-01-07 11:27:25.689022',
-          id: 1,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '월요요'
-        },
-        {
-          applyDate: '2019-02-07 11:27:25.689022',
-          id: 10,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: 'ㅇㅗ로롤로'
-        }
-      ],
-      thu: [
-        {
-          applyDate: '2019-01-17 11:27:25.689022',
-          id: 6,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '목요일'
-        },
-        {
-          applyDate: '2019-01-27 11:27:25.689022',
-          id: 7,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '목오고고곡'
-        }
-      ],
-      tue: [
-        {
-          applyDate: '2019-03-07 11:27:25.689022',
-          id: 2,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '화요일이네'
-        },
-        {
-          applyDate: '2019-04-07 11:27:25.689022',
-          id: 3,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '화가난다'
-        },
-        {
-          applyDate: '2019-01-08 11:27:25.689022',
-          id: 2132,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '수근수근'
-        },
-        {
-          applyDate: '2019-01-08 11:27:25.689022',
-          id: 2136,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '수근수근'
-        },
-        {
-          applyDate: '2019-01-08 11:27:25.689022',
-          id: 21223,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '수근수근'
-        }
-      ],
-      wed: [
-        {
-          applyDate: '2019-01-03 11:27:25.689022',
-          id: 4,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '수요일'
-        },
-        {
-          applyDate: '2019-01-08 11:27:25.689022',
-          id: 5,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '수근수근'
-        },
-        {
-          applyDate: '2019-01-08 11:27:25.689022',
-          id: 5333,
-          musicName: 'aaa',
-          singer: 'aaa',
-          studentName: '수근수근'
-        }
-      ]
-    },
-    isMusicSubmitOpened: false
+      cardsInfo: null,
+      isMusicSubmitOpened: false,
+      loading: false,
+      selectedDate: ''
+    };
+  }
+
+  getCards = async () => {
+    if (this.state.loading) return;
+    this.setState({
+      loading: true
+    });
+
+    try {
+      const response = await getMusicList(getCookie('JWT'));
+      const musicList = response.data;
+
+      this.setState({
+        cardsInfo: musicList
+      });
+    } catch (e) {
+      alert('error');
+      console.log('error: ' + e);
+    }
+    this.setState({
+      loading: false
+    });
   };
+
+  componentDidMount() {
+    this.getCards();
+  }
 
   onApplyMusic = () => {
     this.setState({
@@ -142,52 +72,56 @@ class MusicApplyContainer extends Component {
       alert('음악 제목 혹은 아티스트 이름을 적지 않았습니다.');
       return;
     }
-    //submitMusic(token, day, artist, title);
+    submitMusic(getCookie('JWT'), this.dateToNum(this.props.musicDate), artist, title);
     this.onExitSubmit();
+    this.getCards();
+  };
+
+  dateToNum = date => {
+    switch (date) {
+      case 'mon':
+        return 0;
+      case 'tue':
+        return 1;
+      case 'wed':
+        return 2;
+      case 'thu':
+        return 3;
+      case 'fri':
+        return 4;
+      default:
+        return 0;
+    }
   };
 
   render() {
+    console.log(this.state.cardsInfo);
     const { musicDate, onChangeDate } = this.props;
-    let selectedDate;
     const musicBtnList = this.state.dayType.map((type, i) => {
       let selectedClass = undefined;
-      if (type === musicDate) selectedClass = 'apply--extens--btn--selected';
+      if (type.val === musicDate)
+        selectedClass = 'apply--extens--btn--selected';
       return (
         <ApplyExtensionBtn
-          content={type}
+          content={type.date}
           key={i}
           selected={selectedClass}
           onChangeType={onChangeDate}
+          val={type.val}
         />
       );
     });
-    switch (musicDate) {
-      case '월요일':
-        selectedDate = 'mon';
-        break;
-      case '화요일':
-        selectedDate = 'tue';
-        break;
-      case '수요일':
-        selectedDate = 'wed';
-        break;
-      case '목요일':
-        selectedDate = 'thu';
-        break;
-      case '금요일':
-        selectedDate = 'fri';
-        break;
-      default:
-        selectedDate = 'mon';
-    }
+
     return (
       <div className='apply--music--wrapper'>
         <p className='unselectable apply--title'>기상음악 신청</p>
         <div className='apply--music--btnlist'>{musicBtnList}</div>
-        <MusicCardContainer
-          cardsInfo={this.state.cardsInfo[selectedDate]}
-          onApplyMusic={this.onApplyMusic}
-        />
+        {this.state.cardsInfo !== null && (
+          <MusicCardContainer
+            cardsInfo={this.state.cardsInfo[musicDate]}
+            onApplyMusic={this.onApplyMusic}
+          />
+        )}
         {this.state.isMusicSubmitOpened && (
           <MusicSubmitCard
             onSubmitMusic={this.onSubmitMusic}
