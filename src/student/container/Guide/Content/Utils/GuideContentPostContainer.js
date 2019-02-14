18db528ed8fed56c631c7b699f4daa0d1771e133
khg0712 <k7120792@gmail.com>
2019-02-15 08:36:56 +0900

    [ADD] Guide post,postList container initialize

---
+++ src/student/container/Guide/Content/Utils/GuideContentPostContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './GuideContentPostContainer.scss';
+
+export default class GuideContentPostContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/student/container/Guide/Content/Utils/GuideContentPostLstContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './GuideContentPostLstContainer.scss';
+
+export default class GuideContentPostLstContainer extends Component {
+  render() {
+    return <div></div>;
+  }
+}
