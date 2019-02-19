b46ff4c8e0c925d0605b64767831ed0ebf9a11c1
khg0712 <k7120792@gmail.com>
2019-02-19 17:34:48 +0900

    [MODIFY] guide post list view

--- src/student/component/Guide/GuidePostItem.js
+++ src/student/component/Guide/GuidePostItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './GuidePostItem.scss';
 
-const GuidePostItem = ({ title, postDate }) => {
+const GuidePostItem = ({ title, postDate, id, setDetailPost }) => {
   return (
-    <li className='guide--post--item'>
+    <li className='guide--post--item' onClick={() => setDetailPost(id)}>
       <span className='guide--post--item--title'>{title}</span>
       <span className='guide--post--item--postDate'>{postDate}</span>
     </li>
