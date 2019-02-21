bb12b6d14a07f8f1b7d026ff7636edbf286cfa84
khg0712 <k7120792@gmail.com>
2019-02-21 16:07:35 +0900

    [ADD] apply content container initialize

---
+++ src/student/container/Apply/Utils/ApplyContentContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import './ApplyContentContainer.scss';
+
+export default class ApplyContentContainer extends Component {
+  render() {
+    const { type } = this.props;
+    return <div />;
+  }
+}
