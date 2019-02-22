502d42efff05999c5cb2eef5a8b85f9f57253106
khg0712 <k7120792@gmail.com>
2019-02-22 17:34:17 +0900

    [MODIFY] apply content menu finish

--- src/student/container/Apply/Utils/ApplyContentContainer.js
+++ src/student/container/Apply/Utils/ApplyContentContainer.js
@@ -2,10 +2,77 @@ import React, { Component } from 'react';
 
 import './ApplyContentContainer.scss';
 
+import ApplyContentMenuContainer from './ApplyContentMenuContainer';
+import ApplyContentInnerContainer from './ApplyContentInnerContainer';
+
 export default class ApplyContentContainer extends Component {
+  contentInfo = {
+    extension: {
+      title: '연장신청',
+      menuTitle: '위치선택',
+      menuList: [
+        { content: '가', detail: '가온실' },
+        { content: '나', detail: '나온실' },
+        { content: '다', detail: '다온실' },
+        { content: '라', detail: '라온실' },
+        { content: '2층', detail: '2층 여자 독서실' },
+        { content: '3층', detail: '3층 학교측 독서실' },
+        { content: '3층', detail: '3층 기숙사측 독서실' },
+        { content: '4층', detail: '4층 학교측 독서실' },
+        { content: '4층', detail: '4층 기숙사측 독서실' },
+        { content: '5층', detail: '5층 열린 교실' }
+      ]
+    },
+    goingout: {
+      title: '외출신청',
+      menuTitle: '외출목록',
+      menuList: [{ content: '토', detail: '' }, { content: '평일', detail: '' }]
+    },
+    music: {
+      title: '기상음악',
+      menuTitle: '요일선택',
+      menuList: [
+        { content: '월', detail: '신청가능' },
+        { content: '화', detail: '신청가능' },
+        { content: '수', detail: '신청가능' },
+        { content: '목', detail: '신청가능' },
+        { content: '금', detail: '신청가능' }
+      ]
+    },
+    stay: {
+      title: '잔류신청',
+      menuTitle: '신청목록',
+      menuList: [
+        { content: '금', detail: '금요귀가' },
+        { content: '토', detail: '토요귀가' },
+        { content: '토', detail: '토요귀사' },
+        { content: '잔류', detail: '잔류' }
+      ]
+    }
+  };
+
   render() {
     const { type, menuList, typeList } = this.props;
 
-    return <div className = 'apply--content--wrapper' />;
+    return (
+      <div className='apply--content--outer--wrapper'>
+        <div className='apply--content--wrapper'>
+          <div className='apply--content--left'>
+            <div className='apply--content--title--wrapper'>
+              <span className='apply--content--title'>
+                {this.contentInfo[type].title}
+              </span>
+            </div>
+            <ApplyContentMenuContainer
+              menuTitle={this.contentInfo[type].menuTitle}
+              menuList={this.contentInfo[type].menuList}
+            />
+          </div>
+          <div className='apply--content--right'>
+            <ApplyContentInnerContainer />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/student/container/Apply/Utils/ApplyContentMenuContainer.js
+++ src/student/container/Apply/Utils/ApplyContentMenuContainer.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 
 import './ApplyContentMenuContainer.scss';
 
+import ApplyContentMenuItem from '../../../component/Apply/Utils/ApplyContentMenuItem';
+
 export default class ApplyContentMenuContainer extends Component {
+  state = {
+    selected: 0
+  };
+
+  onClickMenu = index => {
+    this.setState({
+      selected: index
+    });
+  };
+
   render() {
-    const { menuList } = this.props;
-    return <div />;
+    const { menuTitle, menuList } = this.props;
+    const { selected } = this.state;
+    const menuItems = menuList.map((item, i) =>
+      selected === i ? (
+        <ApplyContentMenuItem isSelected='selected' content={item} val={i} onClickMenu={this.onClickMenu} key={i} />
+      ) : (
+        <ApplyContentMenuItem content={item} val={i} onClickMenu={this.onClickMenu} key={i}/>
+      )
+    );
+
+    return (
+      <div className='apply--content--menu--wrapper'>
+        <p className='apply--content--menu--title'>{menuTitle}</p>
+        {menuItems}
+      </div>
+    );
   }
 }
