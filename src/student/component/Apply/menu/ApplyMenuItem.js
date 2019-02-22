9c27d4f9a6062b4e5cd8720669740aaf2d107d1b
khg0712 <k7120792@gmail.com>
2019-02-22 14:05:05 +0900

    [MODIFY] apply menu

--- src/student/component/Apply/menu/ApplyMenuItem.js
+++ src/student/component/Apply/menu/ApplyMenuItem.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './ApplyMenuItem.scss';
 
-const ApplyMenuItem = () => {
-  return <div></div>;
+const ApplyMenuItem = ({content, isSelected}) => {
+  return <div className={`apply--menu--item ${isSelected}`}><p>{content}</p></div>;
 };
 
 export default ApplyMenuItem;
--- src/student/container/Apply/Menu/ApplyMenuContainer.js
+++ src/student/container/Apply/Menu/ApplyMenuContainer.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './ApplyMenuContainer.scss';
 
 import dmsLogo from '../../../../assets/logo/logo.png';
+import ApplyMenuItem from '../../../component/Apply/menu/ApplyMenuItem';
 
 export default class ApplyMenuContainer extends Component {
+  menus = {
+    apply: ['연장', '외출', '잔류', '기상음악']
+  };
   render() {
     const { menuType, selectedMenu } = this.props;
+    const menuItems = this.menus[menuType].map(content => {
+      if (content === selectedMenu)
+        return (
+          <ApplyMenuItem content={content} isSelected={'menu--selected'} />
+        );
+      return <ApplyMenuItem content={content} />;
+    });
     return (
       <div className='apply--menu'>
-        <img src={dmsLogo} alt='dms 로고'/>
+        <img className='apply--menu--logo' src={dmsLogo} alt='dms 로고' />
+        <div className='apply--menu--item--wrapper'>{menuItems}</div>
       </div>
     );
   }
