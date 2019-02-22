82483412a6c9b00c08594158d97fe70fe64502c8
khg0712 <k7120792@gmail.com>
2019-02-22 14:18:18 +0900

    [ADD] apply content inner container

---
+++ src/student/container/Apply/Utils/ApplyContentInnerContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import './ApplyContentInnerContainer.scss';
+
+export default class ApplyContentInnerContainer extends Component {
+  render() {
+    const { type, menuList, typeList } = this.props;
+
+    return <div className = 'apply--content--inner--wrapper' />;
+  }
+}
