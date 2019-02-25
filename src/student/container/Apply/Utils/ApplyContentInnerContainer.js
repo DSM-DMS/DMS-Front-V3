af69253bd6e27a8a0fdcf840e7336ad3866a145a
khg0712 <k7120792@gmail.com>
2019-02-25 23:07:45 +0900

    [MODIFY] inner content container

--- src/student/container/Apply/Utils/ApplyContentInnerContainer.js
+++ src/student/container/Apply/Utils/ApplyContentInnerContainer.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 import './ApplyContentInnerContainer.scss';
+import ApplyTypeSwitch from '../../../component/Apply/Utils/ApplyTypeSwitch';
 
 export default class ApplyContentInnerContainer extends Component {
   render() {
-    const { type, menuList, typeList } = this.props;
+    const { typeList, selectedType, onSelectType } = this.props;
 
-    return <div className = 'apply--content--inner--wrapper' />;
+    return (
+      <div className='apply--content--inner--wrapper'>
+        <div className='apply--content--inner--type--switch--wrapper'>
+          {typeList.length !== 0 ? (
+            <ApplyTypeSwitch
+              content={typeList}
+              selectedType={selectedType}
+              onSelectType={onSelectType}
+            />
+          ) : (
+            undefined
+          )}
+        </div>
+      </div>
+    );
   }
 }
