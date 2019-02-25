f231a67a50a2be0a6c0e1516dd37527cc0e67ec8
khg0712 <k7120792@gmail.com>
2019-02-25 23:08:03 +0900

    [MODIFY] content container

--- src/student/container/Apply/Utils/ApplyContentContainer.js
+++ src/student/container/Apply/Utils/ApplyContentContainer.js
@@ -21,12 +21,21 @@ export default class ApplyContentContainer extends Component {
         { content: '4층', detail: '4층 학교측 독서실' },
         { content: '4층', detail: '4층 기숙사측 독서실' },
         { content: '5층', detail: '5층 열린 교실' }
-      ]
+      ],
+      typeList: [{ content: '11시', val: 11 }, { content: '12시', val: 12 }]
     },
     goingout: {
       title: '외출신청',
       menuTitle: '외출목록',
-      menuList: [{ content: '토', detail: '' }, { content: '평일', detail: '' }]
+      menuList: [
+        { content: '토', detail: '' },
+        { content: '평일', detail: '' }
+      ],
+      typeList: [
+        { content: '토요일', val: 'sat' },
+        { content: '일요일', val: 'sun' },
+        { content: '평일', val: 'week' }
+      ]
     },
     music: {
       title: '기상음악',
@@ -37,7 +46,8 @@ export default class ApplyContentContainer extends Component {
         { content: '수', detail: '신청가능' },
         { content: '목', detail: '신청가능' },
         { content: '금', detail: '신청가능' }
-      ]
+      ],
+      typeList: []
     },
     stay: {
       title: '잔류신청',
@@ -47,13 +57,46 @@ export default class ApplyContentContainer extends Component {
         { content: '토', detail: '토요귀가' },
         { content: '토', detail: '토요귀사' },
         { content: '잔류', detail: '잔류' }
-      ]
+      ],
+      typeList: []
     }
+  };
+
+  state = {
+    selectedType: {
+      extension: 11,
+      goingout: 'sat'
+    },
+    extensionInfo: ['가온실', '-'],
+    stayInfo: '금요귀가'
+  };
+
+  onSelectType = typeVal => {
+    const { type } = this.props;
+    this.setState({
+      selectedType: {
+        ...this.state.selectedType,
+        [type]: typeVal
+      }
+    });
   };
 
   render() {
     const { type, menuList, typeList } = this.props;
-
+    const { selectedType, extensionInfo, stayInfo } = this.state;
+    const applyTag = {
+      extension: (
+        <div className='apply--content--tag--wrapper'>
+          <div className='apply--content--tag'>{extensionInfo[0]}</div>
+          <div className='apply--content--tag second'>{extensionInfo[1]}</div>
+        </div>
+      ),
+      stay: (
+        <div className='apply--content--tag--wrapper'>
+          <div className='apply--content--tag'>{stayInfo}</div>
+        </div>
+      )
+    };
     return (
       <div className='apply--content--outer--wrapper'>
         <div className='apply--content--wrapper'>
@@ -62,6 +105,7 @@ export default class ApplyContentContainer extends Component {
               <span className='apply--content--title'>
                 {this.contentInfo[type].title}
               </span>
+              {applyTag[type]}
             </div>
             <ApplyContentMenuContainer
               menuTitle={this.contentInfo[type].menuTitle}
@@ -69,7 +113,11 @@ export default class ApplyContentContainer extends Component {
             />
           </div>
           <div className='apply--content--right'>
-            <ApplyContentInnerContainer />
+            <ApplyContentInnerContainer
+              typeList={this.contentInfo[type].typeList}
+              selectedType={selectedType[type]}
+              onSelectType={this.onSelectType}
+            />
           </div>
         </div>
       </div>
