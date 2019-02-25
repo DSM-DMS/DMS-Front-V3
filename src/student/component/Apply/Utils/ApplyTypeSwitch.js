0dda3a474fdc2acf295b7a54f13effe63680079c
khg0712 <k7120792@gmail.com>
2019-02-25 23:05:56 +0900

    [MODIFY] type switch

--- src/student/component/Apply/Utils/ApplyTypeSwitch.js
+++ src/student/component/Apply/Utils/ApplyTypeSwitch.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import ApplyTypeSwitchItem from './ApplyTypeSwitchItem';
 
 import './ApplyTypeSwitch.scss';
 
-const ApplyTypeSwitch = () => {
-  return <div></div>;
+const ApplyTypeSwitch = ({ content, selectedType, onSelectType }) => {
+  const items = content.map((item, i) => {
+    if (item.val === selectedType)
+      return (
+        <ApplyTypeSwitchItem
+          content={item.content}
+          onSelectType={onSelectType}
+          val={item.val}
+          key={i}
+          selectedClass='selected'
+        />
+      );
+    return (
+      <ApplyTypeSwitchItem
+        content={item.content}
+        key={i}
+        onSelectType={onSelectType}
+        val={item.val}
+      />
+    );
+  });
+
+  return <ul className='apply--type--switch--wrapper'>{items}</ul>;
 };
 
 export default ApplyTypeSwitch;
