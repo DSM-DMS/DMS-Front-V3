7909916685c9202e15b3d2dd21fba337c65982a7
khg0712 <k7120792@gmail.com>
2019-02-27 16:01:06 +0900

    [MODIFY] apply btn

--- src/student/component/Apply/Utils/ApplyAcceptBtn.js
+++ src/student/component/Apply/Utils/ApplyAcceptBtn.js
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './ApplyAcceptBtn.scss';
 
-const ApplyAcceptBtn = () => {
-  return <div></div>;
+const ApplyAcceptBtn = ({ content, btnClass, onClick, params }) => {
+  return (
+    <div className={`apply--btn ${btnClass}`} onClick={() => onClick(params)}>
+      {content}
+    </div>
+  );
 };
 
 export default ApplyAcceptBtn;
