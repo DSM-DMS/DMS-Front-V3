63381af02f04b236c76185687448efaba42a5c87
khg0712 <k7120792@gmail.com>
2019-02-28 21:26:43 +0900

    [MODIFY] menu unavaiable state setting

--- src/student/container/Apply/Utils/ApplyContentMenuContainer.js
+++ src/student/container/Apply/Utils/ApplyContentMenuContainer.js
@@ -15,6 +15,7 @@ export default class ApplyContentMenuContainer extends Component {
           content={item}
           val={i}
           onClickMenu={onSelectMenu}
+          available={item.available === false ? 'disable' : undefined}
           key={i}
         />
       ) : (
@@ -22,6 +23,7 @@ export default class ApplyContentMenuContainer extends Component {
           content={item}
           val={i}
           onClickMenu={onSelectMenu}
+          available={item.available === false ? 'disable' : undefined}
           key={i}
         />
       )
