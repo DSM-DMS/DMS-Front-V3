f894b24db32bb208706f2517b9c6347b894393b2
khg0712 <k7120792@gmail.com>
2019-03-01 13:40:51 +0900

    [MODIFY] goingout input form finish

--- src/student/component/Apply/content/goingout/GoingoutContent.js
+++ src/student/component/Apply/content/goingout/GoingoutContent.js
@@ -2,8 +2,69 @@ import React from 'react';
 
 import './GoingoutContent.scss';
 
-const GoingoutContent = () => {
-  return <div></div>;
+const GoingoutContent = ({
+  onChangeGoingoutApplication,
+  goingoutApplication
+}) => {
+  return (
+    <div className='apply--content--goingout'>
+      <p>외출 날짜</p>
+      <div className='apply--content--goingout--input--wrapper'>
+        <input
+          name='month'
+          value={goingoutApplication.month}
+          onChange={onChangeGoingoutApplication}
+          className='apply--content--goingout--input'
+        />
+        <span>월</span>
+        <input
+          name='day'
+          value={goingoutApplication.day}
+          onChange={onChangeGoingoutApplication}
+          className='apply--content--goingout--input'
+        />
+        <span>일</span>
+      </div>
+      <p>외출 시각</p>
+      <div className='apply--content--goingout--input--wrapper'>
+        <input
+          name='outHour'
+          value={goingoutApplication.outHour}
+          onChange={onChangeGoingoutApplication}
+          className='apply--content--goingout--input'
+        />
+        <span>시</span>
+        <input
+          name='outMin'
+          value={goingoutApplication.outMin}
+          onChange={onChangeGoingoutApplication}
+          className='apply--content--goingout--input'
+        />
+        <span>분</span>
+        <span> ~ </span>
+        <input
+          name='returnHour'
+          value={goingoutApplication.returnHour}
+          onChange={onChangeGoingoutApplication}
+          className='apply--content--goingout--input'
+        />
+        <span>시</span>
+        <input
+          name='returnMin'
+          value={goingoutApplication.returnMin}
+          onChange={onChangeGoingoutApplication}
+          className='apply--content--goingout--input'
+        />
+        <span>분</span>
+      </div>
+      <p>외출 사유</p>
+      <textarea
+        name='reason'
+        value={goingoutApplication.reason}
+        onChange={onChangeGoingoutApplication}
+      />
+    </div>
+  );
 };
 
 export default GoingoutContent;
