b180a2910de9a320a644c7ba934716df24030057
khg0712 <k7120792@gmail.com>
2019-03-06 19:51:32 +0900

    [MODIFY] api changes

--- src/lib/guideAPI.js
+++ src/lib/guideAPI.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const rootUrl = 'http://ec2.istruly.sexy:5000';
+const rootUrl = 'https://dms-api.istruly.sexy';
 
 export const getNoticeList = () => {
   return axios.get(`${rootUrl}/notice`);
@@ -9,18 +9,10 @@ export const getRuleList = () => {
   return axios.get(`${rootUrl}/rule`);
 };
 
-export const getFaqList = () => {
-  return axios.get(`${rootUrl}/qna`);
-};
-
 export const getNoticeDetailPost = id => {
   return axios.get(`${rootUrl}/notice/${id}`);
 };
 
 export const getRuleDetailPost = id => {
   return axios.get(`${rootUrl}/rule/${id}`);
-};
-
-export const getFaqDetailPost = id => {
-  return axios.get(`${rootUrl}/qna/${id}`);
 };
