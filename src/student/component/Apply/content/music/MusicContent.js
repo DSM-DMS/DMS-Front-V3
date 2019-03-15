20d6e63cc560baeea053286fc97ff5a52d7beec6
gtobio11 <gtobio11@gmail.com>
2019-03-15 09:29:29 +0900

    [MODIFY] music list artist to title

--- src/student/component/Apply/content/music/MusicContent.js
+++ src/student/component/Apply/content/music/MusicContent.js
@@ -8,28 +8,28 @@ const MusicContent = ({
   musicApplication,
   onChangeMusicApplication,
   musicInfo,
-  isOnApply
+  isOnApply,
 }) => {
   if (isOnApply) {
     return (
-      <div className='apply--content--music'>
-        <img className='apply--content--music--icon' src={musicIcon} />
-        <h1 className='apply--content--music--title'>기상 음악 신청하기</h1>
-        <p className='apply--content--music--music_title'>노래 제목</p>
+      <div className="apply--content--music">
+        <img className="apply--content--music--icon" src={musicIcon} alt="" />
+        <h1 className="apply--content--music--title">기상 음악 신청하기</h1>
+        <p className="apply--content--music--music_title">노래 제목</p>
         <input
-          className='apply--content--music--input'
-          name='title'
+          className="apply--content--music--input"
+          name="title"
           value={musicApplication.title}
           onChange={onChangeMusicApplication}
-          autocomplete='off'
+          autocomplete="off"
         />
-        <p className='apply--content--music--artist'>아티스트</p>
+        <p className="apply--content--music--artist">아티스트</p>
         <input
-          className='apply--content--music--input'
-          name='singer'
+          className="apply--content--music--input"
+          name="singer"
           value={musicApplication.singer}
           onChange={onChangeMusicApplication}
-          autocomplete='off'
+          autocomplete="off"
         />
       </div>
     );
--- src/student/component/Apply/content/music/MusicListContent.js
+++ src/student/component/Apply/content/music/MusicListContent.js
@@ -23,14 +23,14 @@ const MusicContent = ({ musicInfo }) => {
   for (let day in musicInfo) {
     const musicList = musicInfo[day].map(music => {
       return (
-        <div className='apply--content--music--list--body' key={music.id}>
-          <div className='apply--content--music--list--day'>
+        <div className="apply--content--music--list--body" key={music.id}>
+          <div className="apply--content--music--list--day">
             {dayConveter(day)}
           </div>
-          <div className='apply--content--music--list--title'>
-            {music.singer}
+          <div className="apply--content--music--list--title">
+            {music.musicName}
           </div>
-          <div className='apply--content--music--list--author'>
+          <div className="apply--content--music--list--author">
             {music.studentName}
           </div>
         </div>
@@ -39,11 +39,11 @@ const MusicContent = ({ musicInfo }) => {
     musicContent.push(musicList);
   }
   return (
-    <div className='apply--content--music--list'>
-      <div className='apply--content--music--list--header'>
-        <div className='apply--content--music--list--day'>요일</div>
-        <div className='apply--content--music--list--title'>노래 제목</div>
-        <div className='apply--content--music--list--author'>작성자</div>
+    <div className="apply--content--music--list">
+      <div className="apply--content--music--list--header">
+        <div className="apply--content--music--list--day">요일</div>
+        <div className="apply--content--music--list--title">노래 제목</div>
+        <div className="apply--content--music--list--author">작성자</div>
       </div>
       {musicContent}
     </div>
