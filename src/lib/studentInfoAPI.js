0c7a60e9784949e0c28414f5826eea4ae2f7a718
gtobio11 <gtobio11@gmail.com>
2019-03-23 23:26:52 +0900

    [MODIFY] axios to axiosWrapper, but i think need to validate data in axiosWrapper

--- src/lib/studentInfoAPI.js
+++ src/lib/studentInfoAPI.js
@@ -1,55 +1,44 @@
-import axios from 'axios';
+import axiosWrapper from './axiosWrapper';
 const rootURL = 'https://api.dms.istruly.sexy';
 
 export const postAuth = (id, pw) =>
-  axios.post(`${rootURL}/account/auth`, {
+  axiosWrapper.post(`${rootURL}/account/auth`, null, null, {
     id: id,
     password: pw,
   });
 
-export const patchPassword = (currentPassword, newPassword, token) =>
-  axios.patch(
-    `${rootURL}/account/pw`,
-    {
-      currentPassword: currentPassword,
-      newPassword: newPassword,
-    },
-    {
-      headers: {
-        Authorization: token,
-      },
-    },
-  );
+export const patchPassword = (
+  currentPassword,
+  newPassword,
+  token,
+  refreshToken,
+) =>
+  axiosWrapper.patch(`${rootURL}/account/pw`, token, refreshToken, {
+    currentPassword: currentPassword,
+    newPassword: newPassword,
+  });
 
 export const postForgotPassword = (id, email) =>
-  axios.post(`${rootURL}/account/pw`, {
+  axiosWrapper.post(`${rootURL}/account/pw`, null, null, {
     id: id,
     email: email,
   });
 
 export const postLogin = (id, pw) =>
-  axios.post(`${rootURL}/account/auth`, {
+  axiosWrapper.post(`${rootURL}/account/auth`, null, null, {
     id: id,
     password: pw,
   });
 
 export const postRegister = (uuid, id, pw) =>
-  axios.post(`${rootURL}/account/signup`, {
+  axiosWrapper.post(`${rootURL}/account/signup`, null, null, {
     uuid: uuid,
     id: id,
     password: pw,
   });
 
-export const getPointCardList = token =>
-  axios.get(`${rootURL}/info/point`, {
-    headers: {
-      Authorization: token,
-    },
-  });
+export const getPointCardList = (token, refreshToken) =>
+  axiosWrapper.get(`${rootURL}/info/point`, token, refreshToken);
 
-export const getBasicDatas = token =>
-  axios.get(`${rootURL}/info/basic`, {
-    headers: {
-      Authorization: token,
-    },
-  });
+export const getBasicDatas = (token, refreshToken) =>
+  axiosWrapper.get(`${rootURL}/info/basic`, token, refreshToken);
