7b2abc12fc578ee3d91212f63deae6ac14e2d640
gtobio11 <gtobio11@gmail.com>
2019-04-21 22:55:28 +0900

    [MODIFY] login logic

--- src/lib/axiosWrapper.js
+++ src/lib/axiosWrapper.js
@@ -4,6 +4,7 @@ import { setCookie, removeCookie } from './cookie';
 const getRefreshTokenURI = '';
 
 function checkValidation(status, refresh, method, path, data) {
+  console.log('test');
   if (status === 403) {
     axios
       .get(getRefreshTokenURI, {
@@ -31,24 +32,30 @@ function checkValidation(status, refresh, method, path, data) {
   } else {
     return false;
   }
+  return 1;
 }
 
 const axiosWrapper = {
   async get(path, token, refresh) {
-    const response = await axios.get(path, {
-      headers: { Authorization: token },
-    });
-
-    const reRequest = checkValidation(
-      response.status,
-      refresh,
-      'get',
-      path,
-      null,
-    );
-    if (reRequest) {
-      return reRequest;
+    console.log(path);
+    let response;
+    try {
+      response = await axios.get(path, {
+        headers: { Authorization: token },
+      });
+    } catch (e) {
+      const reRequest = checkValidation(
+        e.response.status,
+        refresh,
+        'get',
+        path,
+        null,
+      );
+      if (reRequest) {
+        response = reRequest;
+      }
     }
+    console.log(response);
     return response;
   },
   async post(path, token, refresh, data) {
