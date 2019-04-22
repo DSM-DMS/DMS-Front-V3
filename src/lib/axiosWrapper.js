b1eda955d48e9100d5180754c1f11dd6ecdc047f
gtobio11 <gtobio11@gmail.com>
2019-04-22 12:07:29 +0900

    [MODIFY] axios wrapper

--- src/lib/axiosWrapper.js
+++ src/lib/axiosWrapper.js
@@ -3,41 +3,35 @@ import { setCookie, removeCookie } from './cookie';
 
 const getRefreshTokenURI = '';
 
-function checkValidation(status, refresh, method, path, data) {
-  console.log('test');
+async function checkValidation(status, refresh, method, path, data) {
+  let returnVal = false;
   if (status === 403) {
-    axios
-      .get(getRefreshTokenURI, {
+    try {
+      const newTokenRequest = await axios.get(getRefreshTokenURI, {
         headers: { Authorization: `bearer ${refresh}` },
-      })
-      .then(async res => {
-        setCookie('JWT', res.data.accessToken);
-        const reRequest = await axios({
-          method: method,
-          url: path,
-          headers: {
-            Authorization: res.data.accessToken,
-          },
-          data: data,
-        });
-        return reRequest;
-      })
-      .catch(err => {
-        removeCookie('JWT');
-        removeCookie('RFT');
-        return new Promise((resolve, reject) => {
-          reject('expired Token');
-        });
       });
-  } else {
-    return false;
+      setCookie('JWT', newTokenRequest.data.accessToken);
+      returnVal = await axios({
+        method: method,
+        url: path,
+        headers: {
+          Authorization: `bearer ${newTokenRequest.data.accessToken}`,
+        },
+        data: data,
+      });
+    } catch (e) {
+      removeCookie('JWT');
+      removeCookie('RFT');
+      returnVal = new Promise((resolve, reject) => {
+        reject('expired Token');
+      });
+    }
   }
-  return 1;
+  return returnVal;
 }
 
 const axiosWrapper = {
   async get(path, token, refresh) {
-    console.log(path);
     let response;
     try {
       response = await axios.get(path, {
@@ -55,63 +49,65 @@ const axiosWrapper = {
         response = reRequest;
       }
     }
-    console.log(response);
     return response;
   },
   async post(path, token, refresh, data) {
-    const response = await axios.post(path, data, {
-      headers: { Authorization: token },
-    });
-
-    const reRequest = checkValidation(
-      response.status,
-      refresh,
-      'post',
-      path,
-      data,
-    );
-    if (reRequest === 'expired token') {
-      return 'expired token';
-    } else if (reRequest) {
-      return reRequest;
+    let response;
+    try {
+      response = await axios.post(path, data, {
+        headers: { Authorization: token },
+      });
+    } catch (e) {
+      const reRequest = checkValidation(
+        e.response.status,
+        refresh,
+        'post',
+        path,
+        data,
+      );
+      if (reRequest) {
+        response = reRequest;
+      }
     }
     return response;
   },
   async patch(path, token, refresh, data) {
-    const response = await axios.patch(path, data, {
-      headers: { Authorization: token },
-    });
-
-    const reRequest = checkValidation(
-      response.status,
-      refresh,
-      'patch',
-      path,
-      data,
-    );
-    if (reRequest === 'expired token') {
-      return 'expired token';
-    } else if (reRequest) {
-      return reRequest;
+    let response;
+    try {
+      response = await axios.patch(path, data, {
+        headers: { Authorization: token },
+      });
+    } catch (e) {
+      const reRequest = checkValidation(
+        e.response.status,
+        refresh,
+        'patch',
+        path,
+        data,
+      );
+      if (reRequest) {
+        response = reRequest;
+      }
     }
     return response;
   },
   async delete(path, token, refresh, data) {
-    const response = await axios.delete(path, data, {
-      headers: { Authorization: token },
-    });
-
-    const reRequest = checkValidation(
-      response.status,
-      refresh,
-      'delete',
-      path,
-      data,
-    );
-    if (reRequest === 'expired token') {
-      return 'expired token';
-    } else if (reRequest) {
-      return reRequest;
+    let response;
+    try {
+      response = await axios.delete(path, data, {
+        headers: { Authorization: token },
+      });
+    } catch (e) {
+      const reRequest = checkValidation(
+        e.response.status,
+        refresh,
+        'delete',
+        path,
+        data,
+      );
+      if (reRequest) {
+        response = reRequest;
+      }
     }
     return response;
   },
