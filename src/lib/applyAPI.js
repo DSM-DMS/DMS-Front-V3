bd9b295cce8896083eedea31926994c519651066
khg0712 <k7120792@gmail.com>
2019-05-12 22:33:50 +0900

    [MODIFY] apply refresh token

--- src/lib/applyAPI.js
+++ src/lib/applyAPI.js
@@ -1,122 +1,66 @@
 import axios from 'axios';
+import axiosWrapper from './axiosWrapper';
 const rootUrl = 'https://api.dms.istruly.sexy/apply';
 
 export const getExtensionMap = (time, classNum) => {
   return axios.get(`${rootUrl}/extension/map/${time}/${classNum}`);
 };
 
-export const getMyExtensionInfo = (token, time) => {
-  return axios.get(`${rootUrl}/extension/${time}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const getMyExtensionInfo = (token, time, refreshToken) => {
+  return axiosWrapper.get(`${rootUrl}/extension/${time}`, token, refreshToken);
 };
 
-export const deleteExtension = (token, time) => {
-  return axios.delete(`${rootUrl}/extension/${time}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+export const deleteExtension = (token, time, refreshToken) => {
+  return axios.delete(`${rootUrl}/extension/${time}`, token, refreshToken);
 };
 
-export const applyExtension = (token, param) => {
-  return axios.post(
+export const applyExtension = (token, param, refreshToken) => {
+  return axiosWrapper.post(
     `${rootUrl}/extension/${param.time}`,
+    token,
+    refreshToken,
     {
       classNum: param.class + 1,
-      seatNum: param.seat,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
+      seatNum: param.seat
+    }
   );
 };
 
-export const getGoingoutInform = token => {
-  return axios.get(`${rootUrl}/goingout `, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+export const getGoingoutInform = (token, refreshToken) => {
+  return axiosWrapper.get(`${rootUrl}/goingout `, token, refreshToken);
+};
+
+export const deleteGoingoutInform = (token, id, refreshToken) => {
+  return axiosWrapper.delete(`${rootUrl}/goingout`, token, refreshToken, {
+    applyId: id
   });
 };
 
-export const deleteGoingoutInform = (token, id) => {
-  return axios({
-    method: 'DELETE',
-    url: `${rootUrl}/goingout`,
-    data: {
-      applyId: id,
-    },
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+export const postGoingoutInform = (token, dateForm, reason, refreshToken) => {
+  return axiosWrapper.post(`${rootUrl}/goingout `, token, refreshToken, {
+    date: dateForm,
+    reason: reason
   });
 };
 
-// export const postGoingoutInform = (token, goOutDate, returnDate, reason) => {
-export const postGoingoutInform = (token, dateForm, reason) => {
-  return axios.post(
-    `${rootUrl}/goingout `,
-    {
-      // goOutDate,
-      // returnDate,
-      date: dateForm,
-      reason: reason,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
-  );
+export const getMusicList = (token, refreshToken) => {
+  return axios.get(`${rootUrl}/music`, token, refreshToken);
 };
 
-export const getMusicList = token => {
-  return axios.get(`${rootUrl}/music`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+export const submitMusic = (token, day, singer, musicName, refreshToken) => {
+  return axiosWrapper.post(`${rootUrl}/music`, token, refreshToken, {
+    day,
+    singer,
+    musicName
   });
 };
 
-export const submitMusic = (token, day, singer, musicName) => {
-  return axios.post(
-    `${rootUrl}/music`,
-    {
-      day,
-      singer,
-      musicName,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
-  );
+export const getStayInform = (token, refreshToken) => {
+  return axiosWrapper.get(`${rootUrl}/stay`, token, refreshToken);
 };
 
-export const getStayInform = token => {
-  return axios.get(`${rootUrl}/stay`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
+export const submitStayInform = (token, value, refreshToken) => {
+  return axios.post(`${rootUrl}/stay`, token, refreshToken, {
+    value
   });
-};
-
-export const submitStayInform = (token, value) => {
-  return axios.post(
-    `${rootUrl}/stay`,
-    {
-      value,
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    },
-  );
 };
