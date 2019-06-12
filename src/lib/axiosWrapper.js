8b9e7a703e5e93257eba6e134d0a9e56dc01d390
unknown <leaf_cis@naver.com>
2019-06-12 19:10:13 +0900

    New axiosWrapper

--- src/lib/axiosWrapper.js
+++ src/lib/axiosWrapper.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 import { setCookie, removeCookie } from './cookie';
 
-const getRefreshTokenURI = '';
+const getRefreshTokenURI = 'https://api.dms.istruly.sexy/account/refresh';
 
 async function checkValidation(status, refresh, method, path, data, domain) {
   let returnVal = false;
   if (status === 403) {
     try {
-      const newTokenRequest = await axios.get(getRefreshTokenURI, {
+      const newTokenRequest = await axios.post(domain, null, {
         headers: { Authorization: refresh },
       });
       setCookie('JWT', newTokenRequest.data.accessToken);
-      setCookie('RFT', newTokenRequest.data.refreshToken);
-      returnVal = await axios({
+      // setCookie('RFT', newTokenRequest.data.refreshToken);
+      returnVal = axios({
         method: method,
         url: path,
         headers: {
