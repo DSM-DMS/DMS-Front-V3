43069163354345ef5198871c6d217e2c1dd7c3b5
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-04-01 16:54:39 +0900

    [MODIFY] 빌드 파일 수정

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-node scripts/build.js
+node scripts/build.js # ? 빌드
 
 
-yarn javascript-obfuscator ./build
+# yarn javascript-obfuscator ./build # ? 난독화
 
 
-python ./rewrite.py
+# python ./rewrite.py # ? 빌드 파일 삭제 (난독화하면 빌드파일 + 난독화 파일 2개가 같이 존재 따라서 빌드 파일을 삭제 해야함)
+# ? 그러나 현재 ./rewirte.py이 정상적으로 동작하지 않기 때문에 그냥 빌드한 파일만 올림
--- rewrite.py
+++ rewrite.py
@@ -1,27 +1,29 @@
 import os
 import glob
 import time
+import io
+
 dir_path = os.getcwd()
 os.chdir(dir_path + '/build')
 
 for file in list(glob.glob('*-obfuscated*.js')):
-    obReader = open(file, 'r', encoding='UTF8')
-    Reader = open(file.replace("-obfuscated",""), 'w', encoding='UTF8')
+    obReader = io.open(file, 'r', encoding='UTF8')
+    Reader = io.open(file.replace("-obfuscated",""), 'w', encoding='UTF8')
 
     Reader.write(obReader.read())
     Reader.close()
     obReader.close()
 
-    os.remove(os.getcwd()+'\\\\'+file)
-
+    # os.remove(os.getcwd()+"\\\\"+file)
+    os.remove(os.getcwd()+"/"+file)
 
 
 os.chdir(dir_path + '/build/static/js')
 
 
 for file in list(glob.glob('*-obfuscated*.js')):
-    obReader = open(file, 'r', encoding='UTF8')
-    Reader = open(file.replace("-obfuscated",""), 'w', encoding='UTF8')
+    obReader = io.open(file, 'r', encoding='UTF8')
+    Reader = io.open(file.replace("-obfuscated",""), 'w', encoding='UTF8')
 
     Reader.write(obReader.read())
     Reader.close()
