f77b2f482bbf5d549d0f7a0d754a1d8854d812b4
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-17 09:59:58 +0900

    update : add "online going out apply" into header

--- src/student/common/header/Header.js
+++ src/student/common/header/Header.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import logo from '../../../assets/logo/logo-white.png';
-import logogreen from '../../../assets/logo/logo.png';
+import logo from "../../../assets/logo/logo-white.png";
+import logogreen from "../../../assets/logo/logo.png";
 
-import './Header.scss';
+import "./Header.scss";
 
 const Header = ({ buttonList, variableButton, developers }) => {
   return (
     <header
-      className={`${!!developers ? 'developers--page' : ''} header--wrapper`}
+      className={`${!!developers ? "developers--page" : ""} header--wrapper`}
     >
       <Link to="/" className="header--logo">
         <img
@@ -20,6 +20,9 @@ const Header = ({ buttonList, variableButton, developers }) => {
         <span className="header--logo--title">DMS</span>
       </Link>
       <nav className="header--button--list">
+        <a href="https://dsm-sms.com/login" className="header--button--link">
+          평일 외출 신청
+        </a>
         {buttonList}
         {variableButton}
       </nav>
