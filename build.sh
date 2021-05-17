399d4961ae1df255b34fb9a3745f00106ac6a84c
LeeSeongJinCa <dltjdqhr55@gmail.com>
2021-05-17 10:29:02 +0900

    fix : replace # and command for readability

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-node scripts/build.js # ? 빌드
+# ? 빌드
+node scripts/build.js
 
 
-# yarn javascript-obfuscator ./build # ? 난독화
+# ? 난독화
+# yarn javascript-obfuscator ./build
 
-
-# python ./rewrite.py # ? 빌드 파일 삭제 (난독화하면 빌드파일 + 난독화 파일 2개가 같이 존재 따라서 빌드 파일을 삭제 해야함)
+# ? 빌드 파일 삭제 (난독화하면 빌드파일 + 난독화 파일 2개가 같이 존재 따라서 빌드 파일을 삭제 해야함)
 # ? 그러나 현재 ./rewirte.py이 정상적으로 동작하지 않기 때문에 그냥 빌드한 파일만 올림
+# python ./rewrite.py
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-aws s3 sync ./build s3://www.dsm-dms.com --profile=dms-front --delete
 # ? s3 버킷에 배포
+aws s3 sync ./build s3://www.dsm-dms.com --profile=dms-front --delete
 
-aws cloudfront create-invalidation --profile=dms-front --distribution-id E2AZYPH6ANMMCX --paths '/*'
 # ? cloudfront invalidation 생성
+aws cloudfront create-invalidation --profile=dms-front --distribution-id E2AZYPH6ANMMCX --paths '/*'
